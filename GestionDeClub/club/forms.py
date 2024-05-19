4bef7bb0cb1205058298dc3fe907656cdd9b5b21
Pedro Gudiño <rscgudino@gmail.com>
2024-05-19 13:52:52 -0300

    se agrega Agregar Pago Lista de Pago Reporte de Pago  y se modifica estado de socio

--- GestionDeClub/club/forms.py
+++ GestionDeClub/club/forms.py
@@ -1,17 +1,21 @@
 # club/forms.py
 from django import forms
 from .models import Socio
+from .models import Pago
+
 
 class SocioForm(forms.ModelForm):
     class Meta:
         model = Socio
-        fields = ['nombre', 'apellido', 'email', 'telefono', 'direccion']
+        fields = ['nombre', 'apellido', 'email', 'telefono', 'direccion','activo']
         labels = {
             'nombre': 'Nombre',
             'apellido': 'Apellido',
             'email': 'Correo Electrónico',
             'telefono': 'Teléfono',
             'direccion': 'Dirección',
+           
+            'activo':'activo'
         }
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su nombre'}),
@@ -19,4 +23,13 @@ class SocioForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su correo electrónico'}),
             'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su número de teléfono'}),
             'direccion': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su dirección'}),
-        }
+ 
+       }
+        
+        
+# formularios para gestionar los pagos.
+
+class PagoForm(forms.ModelForm):
+    class Meta:
+        model = Pago
+        fields = ['socio', 'fecha_pago', 'monto', 'descripcion']
